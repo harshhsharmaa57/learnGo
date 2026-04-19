3d698eaf2593e53b80e1f7b7f66154c3dc03f6a2
harshhsharmaa <harshhsharmaa57@gmail.com>
2026-04-19 13:31:53 +0530

    more-code

---
+++ 08mySlices/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println("Welcome to video on slices")
+
+
+	var fruitList = []string{"Apple","Tomato","Peach"}
+
+	fmt.Printf("Type of fruitList is: %T\n", fruitList)
+
+	fruitList = append(fruitList, "Mango", "Banana")
+
+	fmt.Println(fruitList)
+
+	fruitList = append(fruitList[1:])
+	fmt.Println(fruitList)
+
+	fruitList = append(fruitList[1:3])
+	fmt.Println(fruitList)
+
+	fruitList = append(fruitList[:3])
+	fmt.Println(fruitList)
+
+	hightScore := make([]int, 4)
+	hightScore[0] = 1
+	hightScore[1] = 12
+	hightScore[2] = 13
+	hightScore[3] = 14
+
+	
+	fmt.Println(hightScore)
+
+	hightScore = append(hightScore, 16,15,10)
+
+	fmt.Println(hightScore)
+
+
+	fmt.Println(sort.IntsAreSorted(hightScore))
+
+	sort.Ints(hightScore)
+
+	fmt.Println(hightScore)
+}
